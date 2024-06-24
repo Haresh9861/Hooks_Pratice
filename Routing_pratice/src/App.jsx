a8efdfb1e333fd8e4a87ae3914391092128df626
Haresh9861 <hareshnayak9861@gmail.com>
2024-06-24 05:52:57 +0000

    adding

--- Routing_pratice/src/App.jsx
+++ Routing_pratice/src/App.jsx
@@ -1,14 +1,38 @@
+import { lazy } from 'react'
 import React from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+const Dashboard = React.lazy(() => import('./Components/Dashboard'))
+const Landing = React.lazy(() => import('./Components/Landing'))
 
 
 function App() {
 
 
   return (
-   <div>
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path="/dashboards" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
 
-   </div>
   )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  return <div>
+    <button onClick={() => {
+      navigate("/")
+    }}>Landing page</button>
+    <button onClick={() => {
+      navigate("/dashboards")
+    }}>Dashboards page</button>
+  </div>
+
 }
 
 export default App
